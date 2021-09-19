27249efb6c5ba5822e694ec435c8f4c1e1a9c1fc
brianmuganda <brian.ashiali@student.moringaschool.com>
2021-09-19 14:10:51 +0300

    tell javascrit not to run until doc loads fully

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
-jQuery('h1').click(function(){
-    alert("This is a heading tag");
-    alert("I told you, THIS IS A HEADER!")
+jQuery(document).ready(function(){
+    jQuery('h1').click(function(){
+        alert("This is a heading tag");
+        alert("I told you, THIS IS A HEADER!")
+    });
+    jQuery('p').click(function(){
+        alert("This is a paragrap")
+    });
+    jQuery('img').click(function(){
+        alert("This is an image")
+    });
 });
-jQuery('p').click(function(){
-    alert("This is a paragrap")
-});
-jQuery('img').click(function(){
-    alert("This is an image")
-})
