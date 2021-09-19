eea65dcaf26f766a3aae5b644805b0bdcca96ec3
brianmuganda <brian.ashiali@student.moringaschool.com>
2021-09-19 13:57:49 +0300

    add click functionality on html elements,using Jqu

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+jQuery('h1').click(function(){
+    alert("This is a heading tag");
+    alert("I told you, THIS IS A HEADER!")
+});
+jQuery('p').click(function(){
+    alert("This is a paragrap")
+});
+jQuery('img').click(function(){
+    alert("This is an image")
+})
